bb0570a1217b206abeb4859685923fcf1a68bf8b
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-16 18:24:26 +0530

    single file uploads successfully

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,22 +5,62 @@ const router = express.Router();
 
 const User = require("../models/user.model");
 
-router.post("",async (req,res)=>{
-    const user = await User.create(req.body);
-    return res.status(201).send(user)
+const {uploadSingle} = require("../middlewares/upload");
+
+const fs = require("fs");
+
+
+
+router.post("", uploadSingle("profile_pic"), async (req,res)=>{
+    try{
+        const user = await User.create({
+            first_name:req.body.first_name,
+            last_name:req.body.last_name,
+            profile_pic:req.file.path
+        });
+        return res.status(200).send(user)
+    }catch(err){
+        res.status(500).json({message:err.message,status:"Failed"})
+    }
+   
 })
 
 router.get("",async (req,res)=>{
-    const users = await User.find().lean().exec();
-    return res.status(201).send(users)
+    try{
+        const users = await User.find().lean().exec();
+        return res.status(201).send(users);
+    }catch(err){
+        return res.status(500).send(err);
+    }
+    
 })
 
-router.patch("",async (req,res)=>{
+
+router.patch("/:id",uploadSingle("profile_image"),async (req, res) => {
+    try {
+        const userone= await User.findById(req.params.id).lean().exec();
+        const user=await User.findByIdAndUpdate(req.params.id,{
+            first_name: req.body?.first_name,
+            last_name: req.body?.last_name,
+            profile_pic: req.file?.path
+        },{new:true}).lean().exec();
+        res.status(200).send(user);
+        if(req.file?.path) {
+            fs.unlinkSync(userone.profile_image)
+        }
+    } catch (err) {
+        res.send(500).json({message: err.message,status:"Failed"});
+    }
 })
 
-router.delete("/:id",async (req,res)=>{
-    const user = await User.findByIdAndDelete(req.params.id).lean().exec();
-    return res.status(201).send(user)
+router.delete("/:id",async (req, res) => {
+    try {
+        const user=await User.findByIdAndDelete(req.params.id).lean().exec();
+        res.send(user);
+        fs.unlinkSync(user.profile_image);
+    } catch (err) {
+        res.send(500).json({message: err.message,status:"Failed"});
+    }
 })
 
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 
-const app = express();
-
-app.use(express.json());
+const app = require("./index")
 
 const connect = require("./configs/db");
 
